0b71f0fe20ea789666b90ca74f192dd293f1e44b
artem_epelman <ded.kropiva@gmail.com>
2023-06-03 08:44:45 +0300

    did task 2.2

--- HW_2/hw_2.py
+++ HW_2/hw_2.py
@@ -40,7 +40,33 @@ add_todo, get_all, delete_todo = notebook()
 # 3) створити функцію котра буде повертати сумму розрядів числа у вигляді строки (також використовуемо типізацію)
 #
 # Приклад:
-# def expanded_form(arg: int):
+def expanded_form(arg) -> str:
+    for i in range(arg):
+        if arg < 100:
+            return f'{str(arg)[0]}{0} + {str(arg)[1]}'
+        elif arg >= 100 and arg < 1000:
+            return f'{str(arg)[0]}{"0" * 2} + {str(arg)[1]}{0} + {str(arg)[2]}'
+        elif arg >= 1000 and arg < 10000:
+            return f'{str(arg)[0]}{"0" * 3} + {str(arg)[1]}{"0" * 2} + {str(arg)[2]}{0} + {str(arg)[3]}'
+        elif arg >= 10000 and arg < 100000:
+            return f'{str(arg)[0]}{"0" * 4} + {str(arg)[1]}{"0" * 3} + {str(arg)[2]}{"0" * 2} + {str(arg)[3]}{"0"} + {str(arg)[4]}'
+        elif arg >= 100000 and arg < 1000000:
+            return f'{str(arg)[0]}{"0" * 5} + {str(arg)[1]}{"0" * 4} + {str(arg)[2]}{"0" * 3} + {str(arg)[3]}{"0"* 2} + {str(arg)[4]}{"0"} + {str(arg)[5]}'
+        elif arg >= 1000000 and arg < 10000000:
+            return f'{str(arg)[0]}{"0" * 6} + {str(arg)[1]}{"0" * 5} + {str(arg)[2]}{"0" * 4} + {str(arg)[3]}{"0"* 3} + {str(arg)[4]}{"0" * 2} + {str(arg)[5]}{"0"} + {str(arg)[6]}'
+# Функція працює з дефектом: Корректно розкладає число на розряди, якщо в числі немає цифри 0
+# питання: Як поставити перевірку - Якщо str(arg)[i] == 0: pass
+
+
+print(expanded_form(22))
+print(expanded_form(233))
+print(expanded_form(2034))
+print(expanded_form(26345))
+print(expanded_form(233456))
+print(expanded_form(2234567))
+
+
+
 
 
 # expanded_form(12) # return '10 + 2'
@@ -50,30 +76,3 @@ add_todo, get_all, delete_todo = notebook()
 #
 # 4) створити декоратор котрий буде підраховувати скільки разів була запущена функція продекорована цим
 # декоратором, та буде виводити це значення після виконання функцій
-
-def decor(func):
-    count_of_call = 0
-
-    def inner():
-        nonlocal count_of_call
-        count_of_call += 1
-        print(count_of_call)
-
-    return inner
-
-
-@decor
-def func1():
-    print('func1')
-
-
-@decor
-def func2():
-    print('func2')
-
-
-func1()
-func1()
-
-func2()
-func2()
